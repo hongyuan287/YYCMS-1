da9b6caa99d2a659f4f4919ad5f29009f7e7abb2
liaoy <liaoy@DESKTOP-68NILIB>
2017-08-23 14:31:21 +0800

    添加Main站点

---
+++ Site.Main/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Site.Main.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Test()
+        {
+            return View();
+        }
+
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        public ActionResult Detial()
+        {
+            return View();
+        }
+    }
+}
